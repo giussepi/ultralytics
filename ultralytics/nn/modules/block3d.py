377b0cdce733330dd1f969607a43515cf656b2c5
Giussepi <giussepexy@gmail.com>
2024-05-02 15:45:03 +0100

    block3d.py -> reusing DFL from block.py

--- ultralytics/nn/modules/block3d.py
+++ ultralytics/nn/modules/block3d.py
@@ -11,6 +11,7 @@ import torch
 from torch import nn
 
 from ultralytics.nn.modules.conv3d import Conv
+from ultralytics.nn.modules.block import DFL
 
 
 __all__ = ('Bottleneck', 'C2f', 'DFL', 'SPPF',)
@@ -59,28 +60,6 @@ class C2f(nn.Module):
         y = list(self.cv1(x).split((self.c, self.c), 1))
         y.extend(m(y[-1]) for m in self.m)
         return self.cv2(torch.cat(y, 1))
-
-
-class DFL(nn.Module):
-    """
-    Integral module of Distribution Focal Loss (DFL).
-
-    Proposed in Generalized Focal Loss https://ieeexplore.ieee.org/document/9792391
-    """
-
-    def __init__(self, c1=16):
-        """Initialize a convolutional layer with a given number of input channels."""
-        super().__init__()
-        self.conv = nn.Conv2d(c1, 1, 1, bias=False).requires_grad_(False)
-        x = torch.arange(c1, dtype=torch.float)
-        self.conv.weight.data[:] = nn.Parameter(x.view(1, c1, 1, 1))
-        self.c1 = c1
-
-    def forward(self, x):
-        """Applies a transformer layer on input tensor 'x' and returns a tensor."""
-        b, c, a = x.shape  # batch, channels, anchors
-        return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
-        # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
 
 
 class SPPF(nn.Module):
