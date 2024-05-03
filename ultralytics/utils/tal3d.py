cd01e95cf1aa90207bc088f865a2a3c4aaa25f6a
Giussepi <giussepexy@gmail.com>
2024-05-03 14:06:22 +0100

    tal3d.py -> dist2bbox

--- ultralytics/utils/tal3d.py
+++ ultralytics/utils/tal3d.py
@@ -18,7 +18,7 @@ TORCH_1_10 = check_version(torch.__version__, '1.10.0')
 
 
 __all__ = [
-    'make_anchors',
+    'make_anchors', 'dist2bbox',
 ]
 
 
@@ -52,3 +52,18 @@ def make_anchors(
         stride_tensor.append(torch.full((h * w * d, 1), stride, dtype=dtype, device=device))
 
     return torch.cat(anchor_points), torch.cat(stride_tensor)
+
+
+def dist2bbox(distance: torch.Tensor, anchor_points: torch.Tensor, xyzwhd: bool = True, dim: int = -1):
+    """Transform distance(ltrb) to box(xyzwhd or xyzxyz)."""
+    lt, rb = distance.chunk(2, dim)  # torch.Size([1, 3, 73]), torch.Size([1, 3, 73])
+    x1y1z1 = anchor_points - lt
+    x2y2z2 = anchor_points + rb
+
+    if xyzwhd:
+        c_xyz = (x1y1z1 + x2y2z2) / 2
+        whd = x2y2z2 - x1y1z1
+
+        return torch.cat((c_xyz, whd), dim)  # xyzwhd bbox torch.Size([1, 6, 73])
+
+    return torch.cat((x1y1z1, x2y2z2), dim)  # xyxy bbox
--- ultralytics/utils/tests/test_tal3d.py
+++ ultralytics/utils/tests/test_tal3d.py
@@ -7,9 +7,10 @@ All rights reserved.
 Original Author: Giussepi Lopez
 """
 
+import pytest
 import torch
 
-from ultralytics.utils.tal3d import make_anchors
+from ultralytics.utils.tal3d import make_anchors, dist2bbox
 
 
 class Testmake_anchors:
@@ -103,3 +104,43 @@ class Testmake_anchors:
         assert actual_strides.shape == expected_strides_shape
         assert torch.equal(actual_strides, expected_strides)
         assert torch.equal(actual_anchors, expected_anchors)
+
+
+class Testdist2bbox:
+
+    @pytest.fixture
+    def get_kwargs(self):
+        d = {
+            'distance': torch.ones(1, 6, 73),
+            'anchor_points': torch.randn(1, 3, 73),
+            'xyzwhd': True,
+            'dim': 1
+        }
+        yield d
+        del d
+
+    def test_1(self, get_kwargs):
+        # xyzwhd = True,
+        kwargs = get_kwargs
+        lt, rb = kwargs['distance'].chunk(2, kwargs['dim'])
+        x1y1z1 = kwargs['anchor_points'] - lt
+        x2y2z2 = kwargs['anchor_points'] + rb
+
+        c_xyz = (x1y1z1 + x2y2z2) / 2
+        whd = x2y2z2 - x1y1z1
+        expected_value = torch.cat((c_xyz, whd), kwargs['dim'])
+        actual_value = dist2bbox(**kwargs)
+
+        assert torch.equal(actual_value, expected_value)
+
+    def test_2(self, get_kwargs):
+        # xyzwhd = False,
+        kwargs = get_kwargs
+        kwargs['xyzwhd'] = False
+        lt, rb = kwargs['distance'].chunk(2, kwargs['dim'])
+        x1y1z1 = kwargs['anchor_points'] - lt
+        x2y2z2 = kwargs['anchor_points'] + rb
+        expected_value = torch.cat((x1y1z1, x2y2z2), kwargs['dim'])
+        actual_value = dist2bbox(**kwargs)
+
+        assert torch.equal(actual_value, expected_value)
